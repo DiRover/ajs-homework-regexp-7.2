26cd9dd84eafc8b0ef794be26911ec9c039ebd40
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-07-26 13:57:24 +0300

    Corrections done

--- src/js/__test__/getTelNumber.test.js
+++ src/js/__test__/getTelNumber.test.js
@@ -16,49 +16,49 @@ test('check telephone number 2', () => {
 
 test('check telephone number 3', () => {
   const tel = '+86 000 000 0000';
-  const expected = '+860000000000';
+  const expected = '+760000000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
 
 test('check telephone number 4', () => {
-  const tel = '+86 00d0 000 0000';
-  const expected = 'Number is not valid';
+  const tel = '+7 960 000 00 0#0';
+  const expected = '+79600000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
 
 test('check telephone number 5', () => {
-  const tel = '+86 00d0 000 0@000';
-  const expected = 'Number is not valid';
+  const tel = '!+86 00d0 000 0@000';
+  const expected = '+760000000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
 
 test('check telephone number 6', () => {
-  const tel = '+86 00Ё0 000 0000';
-  const expected = 'Number is not valid';
+  const tel = 'sdfg+86 00Ё0 000 0000';
+  const expected = '+760000000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
 
 test('check telephone number 7', () => {
-  const tel = '+86 00+0 000 0000';
-  const expected = 'Number is not valid';
+  const tel = '$$%+86 00+0 000 0000';
+  const expected = '+760000000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
 
 test('check telephone number 8', () => {
   const tel = '+86 00@0 000 0000';
-  const expected = 'Number is not valid';
+  const expected = '+760000000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
 
 test('check telephone number 9', () => {
-  const tel = '+86 00#0 000 0000';
-  const expected = 'Number is not valid';
+  const tel = '+86 000 000 0000d';
+  const expected = '+760000000000';
   const received = getTelNumber(tel);
   expect(received).toBe(expected);
 });
--- src/js/getTelNumber.js
+++ src/js/getTelNumber.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-useless-escape */
 export default function getTelNumber(tel) { // экспортируем функцию
-  const repTel = tel.replace(/([\s\)\(\-)])/g, ''); // удаляем пробел, "-" и скобки из номера
-  if (!(repTel.includes('+'))) { // проверяем был ли "+", если нет, то добавляем
+  const repTel = tel.replace(/(\D)/g, ''); // удаляем всё кроме цифр
+  return `+${repTel.replace(/^\d/, '7')}`;
+}
+
+// let tel = '+86 00Ё0 000 0000'
+// getTelNumber(tel)
+
+/* if (!(repTel.includes('+'))) { // проверяем был ли "+", если нет, то добавляем
     if (/\D/.test(repTel)) {
       return 'Number is not valid'; // проверям на корректность номера (содержит что-то кроме цифр)
     }
@@ -10,5 +16,4 @@ export default function getTelNumber(tel) { // экспортируем функ
   if (!(repTel.match(/^\+\d+$/))) {
     return 'Number is not valid'; // проверям на корректность номера (+.....)
   }
-  return repTel; // возвращаем номер телефона, новая переменная, старая не меняется!!!
-}
+  return repTel; // возвращаем номер телефона, новая переменная, старая не меняется!!! */
